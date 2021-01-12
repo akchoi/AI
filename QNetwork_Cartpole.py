fb3600ee5109ec908762ab5340b96a6ae5e18861
akchoi <66295836+akchoi@users.noreply.github.com>
2021-01-12 21:02:42 +0900

    changed

--- QNetwork_Cartpole.py
+++ QNetwork_Cartpole.py
@@ -2,7 +2,8 @@ import gym
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
-a = 2
+
+changed = 1
 env = gym.make("CartPole-v0")
 
 # Input and Output size based on the env
